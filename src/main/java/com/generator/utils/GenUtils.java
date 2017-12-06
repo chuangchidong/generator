d322f4fa56072395cacd4323816ac109b1962e17
chuangchidong <1206214477@qq.com>
2017-12-06 15:49:39 +0800

    vm模板

--- src/main/java/com/generator/utils/GenUtils.java
+++ src/main/java/com/generator/utils/GenUtils.java
@@ -30,9 +30,9 @@ public class GenUtils {
 
     public static List<String> getTemplates(){
         List<String> templates = new ArrayList<String>();
-        templates.add("templates/jpa/Entity.java.vm");
+        templates.add("templates/Entity.java.vm");
         templates.add("templates/Dao.java.vm");
-        templates.add("templates/jpa/Repository.java.vm");
+        templates.add("templates/Repository.java.vm");
         templates.add("templates/Dao.xml.vm");
         templates.add("templates/Service.java.vm");
         templates.add("templates/ServiceImpl.java.vm");
