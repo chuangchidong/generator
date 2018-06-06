b2c14bad421edc8bb8ee3284e31f2713945779a6
chuangchidong <1206214477@qq.com>
2018-06-06 14:39:44 +0800

    跨域配置

---
+++ src/main/java/com/generator/config/CorosConfig.java
@@ -0,0 +1,22 @@
+package com.generator.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * @author zhangzhidong
+ * @date 2018/5/30
+ */
+@Configuration
+public class CorosConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3600);
+    }
+}
