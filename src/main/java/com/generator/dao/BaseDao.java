6cbf9d9961c6cb8fbe62126ff9b68b84bad48b31
chuangchidong <1206214477@qq.com>
2018-09-10 15:05:02 +0800

    通用查询接口类注释

--- src/main/java/com/generator/dao/BaseDao.java
+++ src/main/java/com/generator/dao/BaseDao.java
@@ -11,30 +11,97 @@ import java.util.Map;
  * @date 2017年9月18日 上午9:31:36
  */
 public interface BaseDao<T> {
-	
+	/**
+	 * 保存对象
+	 * @param t
+     */
 	void save(T t);
-	
+
+	/**
+	 * 保存对象
+	 * @param map
+     */
 	void save(Map<String, Object> map);
-	
+
+	/**
+	 * 批量保存对象
+	 * @param list
+     */
 	void saveBatch(List<T> list);
-	
+
+	/**
+	 * 更新对象
+	 * @param t
+	 * @return
+     */
 	int update(T t);
-	
+
+	/**
+	 * 更新对象
+	 * @param map
+	 * @return
+     */
 	int update(Map<String, Object> map);
-	
+
+	/**
+	 * 删除对象
+	 * @param id
+	 * @return
+     */
 	int delete(Object id);
-	
+
+	/**
+	 * 删除对象
+	 * @param map
+	 * @return
+     */
 	int delete(Map<String, Object> map);
-	
+
+	/**
+	 * 删除对象
+	 * @param map
+	 * @return
+     */
+	int deleteMap(Map<String, Object> map);
+
+	/**
+	 * 按照ID批量删除对象
+	 * @param id
+	 * @return
+     */
 	int deleteBatch(Object[] id);
 
+	/**
+	 * 按照ID查询对象
+	 * @param id
+	 * @return
+     */
 	T queryObject(Object id);
-	
+
+	/**
+	 * 按照条件查询对象列表
+	 * @param map
+	 * @return
+     */
 	List<T> queryList(Map<String, Object> map);
-	
-	List<T> queryList(Object id);
-	
+
+	/**
+	 * 按照对象属性查询对象列表
+	 * @param obj
+	 * @return
+     */
+	List<T> queryList(Object obj);
+
+	/**
+	 * 按照字段信息查询数量
+	 * @param map
+	 * @return
+     */
 	int queryTotal(Map<String, Object> map);
 
+	/**
+	 * 查询总数
+	 * @return
+     */
 	int queryTotal();
 }
