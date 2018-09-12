f290d3b330b44397336f3aeddf5785687d37d258
chuangchidong <1206214477@qq.com>
2018-09-13 00:11:33 +0800

    项目中用到的字符串方法

---
+++ src/main/java/com/generator/utils/GenStringUtils.java
@@ -0,0 +1,57 @@
+package com.generator.utils;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.WordUtils;
+
+/**
+ * @author zhangzhidong
+ * @date 2018/9/13
+ */
+public class GenStringUtils {
+
+    /**
+     * 列名转换成Java属性名
+     */
+    public static String columnToJava(String columnName) {
+        return WordUtils.capitalizeFully(columnName, new char[]{'_'}).replace("_", "" );
+    }
+
+    /**
+     * 表名转换成Java类名
+     */
+    public static String tableToJava(String tableName, String tablePrefix) {
+        if (StringUtils.isNotBlank(tablePrefix)) {
+            tableName = tableName.replaceFirst(tablePrefix, "" );
+        }
+        return columnToJava(tableName);
+    }
+
+    /**
+     * 首字母转小写
+     *
+     * @param s 字符串
+     * @return
+     */
+    public static String toLowerCaseFirstOne(String s) {
+        if (Character.isLowerCase(s.charAt(0))) {
+            return s;
+        } else {
+            return (new StringBuilder()).append(Character.toLowerCase(s.charAt(0))).append(s.substring(1)).toString();
+        }
+    }
+
+
+    /**
+     * 首字母转大写
+     *
+     * @param s 字符串
+     * @return
+     */
+    public static String toUpperCaseFirstOne(String s) {
+        if (Character.isUpperCase(s.charAt(0))) {
+            return s;
+        } else {
+            return (new StringBuilder()).append(Character.toUpperCase(s.charAt(0))).append(s.substring(1)).toString();
+        }
+    }
+}
